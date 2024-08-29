d3357c2142cd9e44b5650258d2b1978d611fea9d
Nafi Shahriyar <imnafi404@gmail.com>
2024-08-29 16:46:54 +0600

    Excel export script

---
+++ scraper1/scraper1/scripts/exportColumnsAsExcel.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import json
+
+
+def read_jsonl(file_path):
+    with open(file_path, 'r') as file:
+        data = [json.loads(line) for line in file]
+    return pd.DataFrame(data)
+
+
+file1 = '../data/cleaned/linkedin_jobs_29-08-2024.jsonl'
+file2 = '../data/cleaned/unjobs_29-08-2024.jsonl'
+file3 = '../data/cleaned/vagaservisu_jobs_29-08-2024.jsonl'
+
+df1 = read_jsonl(file1)
+df2 = read_jsonl(file2)
+df3 = read_jsonl(file3)
+
+# Get unique columns from each DataFrame
+columns_set = set(df1.columns) | set(df2.columns) | set(df3.columns)
+unique_columns = sorted(columns_set)
+
+# Create a DataFrame for the output
+output_df = pd.DataFrame(index=unique_columns, columns=['Linkedin', 'UN Jobs', 'Vagaservisu'])
+
+# Fill the DataFrame with 'yes' or 'no'
+for column in unique_columns:
+    output_df.at[column, 'Linkedin'] = 'yes' if column in df1.columns else 'no'
+    output_df.at[column, 'UN Jobs'] = 'yes' if column in df2.columns else 'no'
+    output_df.at[column, 'Vagaservisu'] = 'yes' if column in df3.columns else 'no'
+
+# Export to Excel
+output_df.to_excel('../data/cleaned/columns.xlsx', sheet_name='Columns')
+
+print("Excel file 'job_posting_columns.xlsx' has been created.")
---
+++ scraper1/scraper1/scripts/exportDataAsExcel.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import json
+
+
+def read_jsonl(file_path):
+    with open(file_path, 'r') as file:
+        data = [json.loads(line) for line in file]
+    return pd.DataFrame(data)
+
+
+file1 = '../data/cleaned/linkedin_jobs_29-08-2024.jsonl'
+file2 = '../data/cleaned/unjobs_27-08-2024.jsonl'
+file3 = '../data/cleaned/vagaservisu_jobs_27-08-2024.jsonl'
+
+# Read the JSONL files
+df1 = read_jsonl(file1)
+df2 = read_jsonl(file2)
+df3 = read_jsonl(file3)
+
+# Add a title row for each DataFrame
+df1.columns = pd.MultiIndex.from_product([['LinkedIn'], df1.columns])
+df2.columns = pd.MultiIndex.from_product([['UN Jobs'], df2.columns])
+df3.columns = pd.MultiIndex.from_product([['Vagaservisu'], df3.columns])
+
+# Combine the DataFrames side by side
+combined_df = pd.concat([df1, df2, df3], axis=1)
+
+# Export to Excel
+output_file = '../data/cleaned/job_data.xlsx'
+combined_df.to_excel(output_file)
+
+print(f"Data has been exported to {output_file}")
