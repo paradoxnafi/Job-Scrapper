95b7c6f0eb783085c84e5c0b70b94c2d88ff0705
Nafi Shahriyar <imnafi404@gmail.com>
2024-10-02 15:41:01 +0600

    LinkedIn spider headless

--- scraper1/scraper1/spiders/linkedin_spider.py
+++ scraper1/scraper1/spiders/linkedin_spider.py
@@ -14,8 +14,8 @@ class LinkedInJobSpider(scrapy.Spider):
 
         # Set up Selenium WebDriver with Chrome
         chrome_options = Options()
-        # chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
         # chrome_options.add_argument("--no-sandbox")
         self.driver = webdriver.Chrome(options=chrome_options)
 
