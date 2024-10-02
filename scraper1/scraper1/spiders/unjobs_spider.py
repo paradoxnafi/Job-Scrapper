c9b87d932f6f57a526a20b54c5bc15d27d3e0dc0
Nafi Shahriyar <imnafi404@gmail.com>
2024-10-02 12:15:22 +0600

    removed selnaium dependancy

--- scraper1/scraper1/spiders/unjobs_spider.py
+++ scraper1/scraper1/spiders/unjobs_spider.py
@@ -1,8 +1,4 @@
 import scrapy
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from scrapy.selector import Selector
-import time
 from datetime import datetime
 
 
@@ -33,23 +29,9 @@ class UnjobsSpider(scrapy.Spider):
     allowed_domains = ["unjobs.org"]
     start_urls = ["https://unjobs.org/duty_stations/dil"]
 
-    def __init__(self, *args, **kwargs):
-        super(UnjobsSpider, self).__init__(*args, **kwargs)
-
-        # Set up Selenium WebDriver with Chrome
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        chrome_options.add_argument("--disable-gpu")
-        # chrome_options.add_argument("--no-sandbox")
-        self.driver = webdriver.Chrome(options=chrome_options)
-
     def parse(self, response):
-        self.driver.get(response.url)
-
-        time.sleep(5)
-
-        sel = Selector(text=self.driver.page_source)
-        job_posts = sel.css('.job[id]')
+        # Extract job posts using CSS selectors
+        job_posts = response.css('.job[id]')
 
         for job in job_posts:
             title = job.css('.jtitle::text').get()
